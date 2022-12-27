4adbe702381b5db2d21b6e4b4d9e2d0a47adb6f3
Alex Breen <alexander.breen@gmail.com>
2022-12-27 17:00:22 -0500

    Initial commit

---
+++ Term.java
@@ -0,0 +1,273 @@
+/*
+ * Term.java
+ *
+ * A simple utility class for ANSI terminals.
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Term {
+    public static void main(String[] args) {
+        colorTest(Color.COLORS, Background.BACKGROUNDS);
+        modeTest();
+        colorTest(Color.BRIGHT_COLORS, Background.BRIGHT_BACKGROUNDS);
+    }
+
+    private static void colorTest(Color[] colors, Background[] backgrounds) {
+        int i = 0;
+        for (Color c : colors) {
+            for (Background b : backgrounds) {
+                System.out.print(
+                    Term.setModes(b, c) +
+                    String.format(" %x/%x ", c.getCode(), b.getCode()) +
+                    Term.unsetModes(b, c)
+                );
+
+                if (++i % 8 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    private static void modeTest() {
+        int i = 0;
+        for (Mode m : Mode.NON_COLOR_MODES) {
+            System.out.print(
+                Term.setMode(m) +
+                String.format(" mode%d ", m.getCode()) +
+                Term.unsetMode(m)
+            );
+
+            if (++i % 8 == 0) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static final String BEL = "\u0007";
+    public static final String BS = "\u0008";
+    public static final String HT = "\u0009";
+    public static final String LF = "\r";
+    public static final String VT = "\u000b";
+    public static final String FF = "\u000c";
+    public static final String CR = "\n";
+    public static final String ESC = "\u001b";
+    public static final String DEL = "\u007f";
+
+    public static final String CSI = ESC + "[";
+
+    public static String setMode(Mode m) {
+        return CSI + m.getCode() + "m";
+    }
+
+    public static String unsetMode(Mode m) {
+        return CSI + getUnsetCode(m) + "m";
+    }
+
+    private static int getUnsetCode(Mode m) {
+        if (m instanceof Background) {
+            return Background.DEFAULT.getCode();
+        }
+        if (m instanceof Color) {
+            return Color.DEFAULT.getCode();
+        }
+        if (m == Mode.BOLD || m == Mode.DIM) {
+            return 22;
+        }
+        return 20 + m.getCode();
+    }
+
+    public static String setModes(Mode... modes) {
+        return buildEscapeSequence(Mode::getCode, modes);
+
+    }
+
+    public static String unsetModes(Mode... modes) {
+        return buildEscapeSequence(Term::getUnsetCode, modes);
+    }
+
+    public static String buildEscapeSequence(Function<Mode, Integer> f, Mode... modes) {
+        return graphicsModeEscape(
+            Arrays.stream(modes).map(f).collect(Collectors.toList())
+        );
+    }
+
+    private static String graphicsModeEscape(List<Integer> codes) {
+        return CSI + codes.stream().map(Objects::toString)
+                          .collect(Collectors.joining(";")) + "m";
+    }
+
+    public static String reset() { return CSI + 0 + "m"; }
+    public static String resetColor() { return unsetMode(Color.RED); }
+    public static String resetBackground() { return unsetMode(Background.RED); }
+
+    public static String bold(String s) { return enclose(s, Mode.BOLD); }
+    public static String dim(String s) { return enclose(s, Mode.DIM); }
+    public static String italic(String s) { return enclose(s, Mode.ITALIC); }
+    public static String underline(String s) { return enclose(s, Mode.UNDERLINE); }
+    public static String blink(String s) { return enclose(s, Mode.BLINK); }
+    public static String invert(String s) { return enclose(s, Mode.INVERT); }
+    public static String hidden(String s) { return enclose(s, Mode.HIDDEN); }
+    public static String strikethrough(String s) { return enclose(s, Mode.STRIKETHROUGH); }
+
+    public static String black(String s) { return enclose(s, Color.BLACK); }
+    public static String red(String s) { return enclose(s, Color.RED); }
+    public static String green(String s) { return enclose(s, Color.GREEN); }
+    public static String yellow(String s) { return enclose(s, Color.YELLOW); }
+    public static String blue(String s) { return enclose(s, Color.BLUE); }
+    public static String magenta(String s) { return enclose(s, Color.MAGENTA); }
+    public static String cyan(String s) { return enclose(s, Color.CYAN); }
+    public static String white(String s) { return enclose(s, Color.WHITE); }
+
+    public static String brBlack(String s) { return enclose(s, Color.BLACK_BRIGHT); }
+    public static String brRed(String s) { return enclose(s, Color.RED_BRIGHT); }
+    public static String brGreen(String s) { return enclose(s, Color.GREEN_BRIGHT); }
+    public static String brYellow(String s) { return enclose(s, Color.YELLOW_BRIGHT); }
+    public static String brBlue(String s) { return enclose(s, Color.BLUE_BRIGHT); }
+    public static String brMagenta(String s) { return enclose(s, Color.MAGENTA_BRIGHT); }
+    public static String brCyan(String s) { return enclose(s, Color.CYAN_BRIGHT); }
+    public static String brWhite(String s) { return enclose(s, Color.WHITE_BRIGHT); }
+
+    public static String bgBlack(String s) { return enclose(s, Background.BLACK); }
+    public static String bgRed(String s) { return enclose(s, Background.RED); }
+    public static String bgGreen(String s) { return enclose(s, Background.GREEN); }
+    public static String bgYellow(String s) { return enclose(s, Background.YELLOW); }
+    public static String bgBlue(String s) { return enclose(s, Background.BLUE); }
+    public static String bgMagenta(String s) { return enclose(s, Background.MAGENTA); }
+    public static String bgCyan(String s) { return enclose(s, Background.CYAN); }
+    public static String bgWhite(String s) { return enclose(s, Background.WHITE); }
+
+    public static String bgBrBlack(String s) { return enclose(s, Background.BLACK_BRIGHT); }
+    public static String bgBrRed(String s) { return enclose(s, Background.RED_BRIGHT); }
+    public static String bgBrGreen(String s) { return enclose(s, Background.GREEN_BRIGHT); }
+    public static String bgBrYellow(String s) { return enclose(s, Background.YELLOW_BRIGHT); }
+    public static String bgBrBlue(String s) { return enclose(s, Background.BLUE_BRIGHT); }
+    public static String bgBrMagenta(String s) { return enclose(s, Background.MAGENTA_BRIGHT); }
+    public static String bgBrCyan(String s) { return enclose(s, Background.CYAN_BRIGHT); }
+    public static String bgBrWhite(String s) { return enclose(s, Background.WHITE_BRIGHT); }
+
+    public static String cursorHome() { return CSI + "H"; }
+    public static String cursorTo(int line, int column) { return CSI + line + ";" + column + "H"; }
+    public static String cursorUp(int numLines) { return CSI + numLines + "A"; }
+    public static String cursorDown(int numLines) { return CSI + numLines + "B"; }
+    public static String cursorRight(int numColumns) { return CSI + numColumns + "C"; }
+    public static String cursorLeft(int numColumns) { return CSI + numColumns + "D"; }
+    public static String cursorDownLines(int numLines) { return CSI + numLines + "E"; }
+    public static String cursorUpLines(int numLines) { return CSI + numLines + "F"; }
+    public static String cursorToColumn(int column) { return CSI + column + "G"; }
+    public static String cursorScrollUp() { return ESC + "M"; }
+    public static String cursorSave() { return ESC + 7; }
+    public static String cursorRestore() { return ESC + 8; }
+
+    public static String eraseToEnd() { return CSI + "0J"; }
+    public static String eraseToBeginning() { return CSI + "1J"; }
+    public static String eraseScreen() { return CSI + "2J"; }
+    public static String eraseToEndOfLine() { return CSI + "0K"; }
+    public static String eraseToBeginningOfLine() { return CSI + "1K"; }
+    public static String eraseLine() { return CSI + "2K"; }
+
+    public static String saveScreen() { return CSI + "?47h"; }
+    public static String restoreScreen() { return CSI + "?47l"; }
+
+    private static String enclose(String s, Mode m) {
+        return setMode(m) + s + unsetMode(m);
+    }
+
+    public static Function<String, String> compose(Mode... modes) {
+        return str -> setModes(modes) + str + unsetModes(modes);
+    }
+}
+
+abstract class Code {
+    private int code;
+    public Code(int code) { this.code = code; }
+    public int getCode() { return code; }
+}
+
+class Mode extends Code {
+    public static final Mode BOLD = new Mode(1);
+    public static final Mode DIM = new Mode(2);
+    public static final Mode ITALIC = new Mode(3);
+    public static final Mode UNDERLINE = new Mode(4);
+    public static final Mode BLINK = new Mode(5);
+    public static final Mode INVERT = new Mode(7);
+    public static final Mode HIDDEN = new Mode(8);
+    public static final Mode STRIKETHROUGH = new Mode(9);
+
+    public Mode(int code) { super(code); }
+
+    public static final Mode[] NON_COLOR_MODES = {
+        BOLD, DIM, ITALIC, UNDERLINE, BLINK, INVERT, HIDDEN, STRIKETHROUGH
+    };
+}
+
+class Color extends Mode {
+    public static final Color DEFAULT = new Color(39);
+
+    public static final Color BLACK = new Color(30);
+    public static final Color RED = new Color(31);
+    public static final Color GREEN = new Color(32);
+    public static final Color YELLOW = new Color(33);
+    public static final Color BLUE = new Color(34);
+    public static final Color MAGENTA = new Color(35);
+    public static final Color CYAN = new Color(36);
+    public static final Color WHITE = new Color(37);
+
+    public static final Color BLACK_BRIGHT = new Color(90);
+    public static final Color RED_BRIGHT = new Color(91);
+    public static final Color GREEN_BRIGHT = new Color(92);
+    public static final Color YELLOW_BRIGHT = new Color(93);
+    public static final Color BLUE_BRIGHT = new Color(94);
+    public static final Color MAGENTA_BRIGHT = new Color(95);
+    public static final Color CYAN_BRIGHT = new Color(96);
+    public static final Color WHITE_BRIGHT = new Color(97);
+
+    public Color(int code) { super(code); }
+
+    public static final Color[] COLORS = {
+        BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+    };
+
+    public static final Color[] BRIGHT_COLORS = {
+        BLACK_BRIGHT, RED_BRIGHT, GREEN_BRIGHT, YELLOW_BRIGHT, BLUE_BRIGHT,
+        MAGENTA_BRIGHT, CYAN_BRIGHT, WHITE_BRIGHT
+    };
+}
+
+class Background extends Color {
+    public static final Background DEFAULT = new Background(49);
+
+    public static final Background BLACK = new Background(40);
+    public static final Background RED = new Background(41);
+    public static final Background GREEN = new Background(42);
+    public static final Background YELLOW = new Background(43);
+    public static final Background BLUE = new Background(44);
+    public static final Background MAGENTA = new Background(45);
+    public static final Background CYAN = new Background(46);
+    public static final Background WHITE = new Background(47);
+
+    public static final Background BLACK_BRIGHT = new Background(100);
+    public static final Background RED_BRIGHT = new Background(101);
+    public static final Background GREEN_BRIGHT = new Background(102);
+    public static final Background YELLOW_BRIGHT = new Background(103);
+    public static final Background BLUE_BRIGHT = new Background(104);
+    public static final Background MAGENTA_BRIGHT = new Background(105);
+    public static final Background CYAN_BRIGHT = new Background(106);
+    public static final Background WHITE_BRIGHT = new Background(107);
+
+    public Background(int code) { super(code); }
+
+    public static final Background[] BACKGROUNDS = {
+        BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+    };
+
+    public static final Background[] BRIGHT_BACKGROUNDS = {
+        BLACK_BRIGHT, RED_BRIGHT, GREEN_BRIGHT, YELLOW_BRIGHT, BLUE_BRIGHT,
+        MAGENTA_BRIGHT, CYAN_BRIGHT, WHITE_BRIGHT
+    };
+}
---
+++ TermTest.java
@@ -0,0 +1,188 @@
+import java.util.function.Function;
+
+public class TermTest {
+    private static final String ESC = "\u001b";
+
+    @Test
+    public static void testResets() {
+        assert Term.reset().equals(ESC + "[0m");
+        assert Term.resetColor().equals(ESC + "[39m");
+        assert Term.resetBackground().equals(ESC + "[49m");
+    }
+
+    @Test
+    public static void testModes() {
+        {
+            String expected = ESC + "[1mtest" + ESC + "[22m";
+            assert Term.bold("test").equals(expected);
+            assert (Term.setMode(Mode.BOLD) + "test" + Term.unsetMode(Mode.BOLD))
+                       .equals(expected);
+        }
+        {
+            String expected = ESC + "[2mtest" + ESC + "[22m";
+            assert Term.dim("test").equals(expected);
+            assert (Term.setMode(Mode.DIM) + "test" + Term.unsetMode(Mode.DIM))
+                       .equals(expected);
+        }
+        {
+            String expected = ESC + "[3mtest" + ESC + "[23m";
+            assert Term.italic("test").equals(expected);
+            assert (Term.setMode(Mode.ITALIC) + "test" + Term.unsetMode(Mode.ITALIC))
+                       .equals(expected);
+        }
+        {
+            String expected = ESC + "[4mtest" + ESC + "[24m";
+            assert Term.underline("test").equals(expected);
+            assert (Term.setMode(Mode.UNDERLINE) +
+                    "test" +
+                    Term.unsetMode(Mode.UNDERLINE)).equals(expected);
+        }
+        {
+            String expected = ESC + "[5mtest" + ESC + "[25m";
+            assert Term.blink("test").equals(expected);
+            assert (Term.setMode(Mode.BLINK) +
+                    "test" +
+                    Term.unsetMode(Mode.BLINK)).equals(expected);
+        }
+        {
+            String expected = ESC + "[7mtest" + ESC + "[27m";
+            assert Term.invert("test").equals(expected);
+            assert (Term.setMode(Mode.INVERT) +
+                    "test" +
+                    Term.unsetMode(Mode.INVERT)).equals(expected);
+        }
+        {
+            String expected = ESC + "[8mtest" + ESC + "[28m";
+            assert Term.hidden("test").equals(expected);
+            assert (Term.setMode(Mode.HIDDEN) +
+                    "test" +
+                    Term.unsetMode(Mode.HIDDEN)).equals(expected);
+        }
+        {
+            String expected = ESC + "[9mtest" + ESC + "[29m";
+            assert Term.strikethrough("test").equals(expected);
+            assert (Term.setMode(Mode.STRIKETHROUGH) +
+                    "test" +
+                    Term.unsetMode(Mode.STRIKETHROUGH)).equals(expected);
+        }
+    }
+
+    @Test
+    public static void testColors() {
+        assert Term.black("test").equals(ESC + "[30mtest" + ESC + "[39m");
+        assert Term.red("test").equals(ESC + "[31mtest" + ESC + "[39m");
+        assert Term.green("test").equals(ESC + "[32mtest" + ESC + "[39m");
+        assert Term.yellow("test").equals(ESC + "[33mtest" + ESC + "[39m");
+        assert Term.blue("test").equals(ESC + "[34mtest" + ESC + "[39m");
+        assert Term.magenta("test").equals(ESC + "[35mtest" + ESC + "[39m");
+        assert Term.cyan("test").equals(ESC + "[36mtest" + ESC + "[39m");
+        assert Term.white("test").equals(ESC + "[37mtest" + ESC + "[39m");
+    }
+
+    @Test
+    public static void testColorsWithSet() {
+        {
+            String expected = ESC + "[30mtest" + ESC + "[39m";
+            assert (
+                Term.setMode(Color.BLACK) + "test" + Term.unsetMode(Color.BLACK)
+            ).equals(expected);
+        }
+        {
+            String expected = ESC + "[31mtest" + ESC + "[39m";
+            assert (
+                Term.setMode(Color.RED) + "test" + Term.unsetMode(Color.RED)
+            ).equals(expected);
+        }
+        {
+            String expected = ESC + "[32mtest" + ESC + "[39m";
+            assert (
+                Term.setMode(Color.GREEN) + "test" + Term.unsetMode(Color.GREEN)
+            ).equals(expected);
+        }
+        {
+            String expected = ESC + "[33mtest" + ESC + "[39m";
+            assert (
+                Term.setMode(Color.YELLOW) + "test" + Term.unsetMode(Color.YELLOW)
+            ).equals(expected);
+        }
+        {
+            String expected = ESC + "[34mtest" + ESC + "[39m";
+            assert (
+                Term.setMode(Color.BLUE) + "test" + Term.unsetMode(Color.BLUE)
+            ).equals(expected);
+        }
+        {
+            String expected = ESC + "[35mtest" + ESC + "[39m";
+            assert (
+                Term.setMode(Color.MAGENTA) + "test" + Term.unsetMode(Color.MAGENTA)
+            ).equals(expected);
+        }
+        {
+            String expected = ESC + "[36mtest" + ESC + "[39m";
+            assert (
+                Term.setMode(Color.CYAN) + "test" + Term.unsetMode(Color.CYAN)
+            ).equals(expected);
+        }
+        {
+            String expected = ESC + "[37mtest" + ESC + "[39m";
+            assert (
+                Term.setMode(Color.WHITE) + "test" + Term.unsetMode(Color.WHITE)
+            ).equals(expected);
+        }
+    }
+
+    @Test
+    public static void testBrightColors() {
+        assert Term.brBlack("test").equals(ESC + "[90mtest" + ESC + "[39m");
+        assert Term.brWhite("test").equals(ESC + "[97mtest" + ESC + "[39m");
+        assert Term.brRed("test").equals(ESC + "[91mtest" + ESC + "[39m");
+        assert Term.brGreen("test").equals(ESC + "[92mtest" + ESC + "[39m");
+        assert Term.brBlue("test").equals(ESC + "[94mtest" + ESC + "[39m");
+        assert Term.brCyan("test").equals(ESC + "[96mtest" + ESC + "[39m");
+        assert Term.brMagenta("test").equals(ESC + "[95mtest" + ESC + "[39m");
+        assert Term.brYellow("test").equals(ESC + "[93mtest" + ESC + "[39m");
+    }
+
+    @Test
+    public static void testBackgroundColors() {
+        assert Term.bgBlack("test").equals(ESC + "[40mtest" + ESC + "[49m");
+        assert Term.bgWhite("test").equals(ESC + "[47mtest" + ESC + "[49m");
+        assert Term.bgRed("test").equals(ESC + "[41mtest" + ESC + "[49m");
+        assert Term.bgGreen("test").equals(ESC + "[42mtest" + ESC + "[49m");
+        assert Term.bgBlue("test").equals(ESC + "[44mtest" + ESC + "[49m");
+        assert Term.bgCyan("test").equals(ESC + "[46mtest" + ESC + "[49m");
+        assert Term.bgMagenta("test").equals(ESC + "[45mtest" + ESC + "[49m");
+        assert Term.bgYellow("test").equals(ESC + "[43mtest" + ESC + "[49m");
+    }
+
+    @Test
+    public static void testBrightBackgroundColors() {
+        assert Term.bgBrBlack("test").equals(ESC + "[100mtest" + ESC + "[49m");
+        assert Term.bgBrWhite("test").equals(ESC + "[107mtest" + ESC + "[49m");
+        assert Term.bgBrRed("test").equals(ESC + "[101mtest" + ESC + "[49m");
+        assert Term.bgBrGreen("test").equals(ESC + "[102mtest" + ESC + "[49m");
+        assert Term.bgBrBlue("test").equals(ESC + "[104mtest" + ESC + "[49m");
+        assert Term.bgBrCyan("test").equals(ESC + "[106mtest" + ESC + "[49m");
+        assert Term.bgBrMagenta("test").equals(ESC + "[105mtest" + ESC + "[49m");
+        assert Term.bgBrYellow("test").equals(ESC + "[103mtest" + ESC + "[49m");
+    }
+
+    @Test
+    public static void testCompose() {
+        Function<String, String> style1 = Term.compose(Color.BLUE, Background.YELLOW);
+        Function<String, String> style2 = Term.compose(
+            Mode.BOLD, Mode.ITALIC, Color.WHITE_BRIGHT, Background.RED_BRIGHT
+        );
+
+        assert style1.apply("test").equals(
+            ESC + "[34;43m" +
+            "test" +
+            ESC + "[39;49m"
+        );
+        assert style2.apply("test").equals(
+            ESC + "[1;3;97;101m" +
+            "test" +
+            ESC + "[22;23;39;49m"
+        );
+    }
+}
